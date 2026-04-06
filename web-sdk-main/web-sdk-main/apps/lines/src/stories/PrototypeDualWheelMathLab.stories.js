791c499f3eef4a660a73efe639b24a5e661d43f0
whaklgjndo <stakeemail.23@gmail.com>
2026-04-06 18:49:49 -0500

    Add Storybook entry for dual wheel math lab

---
+++ web-sdk-main/web-sdk-main/apps/lines/src/stories/PrototypeDualWheelMathLab.stories.js
@@ -0,0 +1,13 @@
+import DualWheelMathLab from './components/DualWheelMathLab.svelte';
+
+export default {
+	title: 'PROTOTYPES/Dual Wheel Math Lab',
+	parameters: {
+		layout: 'fullscreen',
+	},
+	render: () => ({
+		Component: DualWheelMathLab,
+	}),
+};
+
+export const Playground = {};
