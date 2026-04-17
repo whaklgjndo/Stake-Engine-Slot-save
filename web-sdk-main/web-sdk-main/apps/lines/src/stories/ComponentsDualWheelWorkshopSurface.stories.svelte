898ad0cb117283a70fc7e5e8f58eb87f6c07c696
whaklgjndo <stakeemail.23@gmail.com>
2026-04-16 23:06:42 -0500

    Add Storybook entry for dual wheel workshop surface

---
+++ web-sdk-main/web-sdk-main/apps/lines/src/stories/ComponentsDualWheelWorkshopSurface.stories.svelte
@@ -0,0 +1,13 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import DualWheelWorkshopSurface from './components/DualWheelWorkshopSurface.svelte';
+
+	const { Story } = defineMeta({
+		title: 'PROTOTYPES/Dual Wheel Workshop Surface',
+		component: DualWheelWorkshopSurface,
+	});
+</script>
+
+<Story name="surface">
+	<DualWheelWorkshopSurface />
+</Story>
